ad0de13e2ef235c57cf7d88a64a1d219e661dca6
JAGT1806 <jagt1806@gmail.com>
2026-08-08 21:58:13 -0500

    feat: prueba

---
+++ src/main/java/com/example/pruebarelease/PruebaReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebarelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaReleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pruebarelease/PruebaReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebarelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
